bd7163dfcf5b8aa77c5bb65bdc8cc8bb3549e25a
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2024-02-10 15:52:51 -0800

    Exportando módulos para serem usados como dependência em outros pacotes.

---
+++ fontes/index.ts
@@ -0,0 +1,6 @@
+export * from './analisador-semantico';
+export * from './avaliador-sintatico';
+export * from './bibliotecas';
+export * from './formatador';
+export * from './interpretador';
+export * from './lexador';
